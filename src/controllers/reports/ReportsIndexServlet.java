aaa0807413211e0102bf49f276e269e2055249a7
katsuya.muramatsu <9ketuki3@gmail.com>
2019-06-08 01:23:50 +0900

    Modify top page

--- src/controllers/reports/ReportsIndexServlet.java
+++ src/controllers/reports/ReportsIndexServlet.java
@@ -11,8 +11,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import models.Employee;
 import models.Report;
 import utils.DBUtil;
+
 /**
  * Servlet implementation class ReportsIndexServlet
  */
@@ -31,36 +33,43 @@ public class ReportsIndexServlet extends HttpServlet {
     /**
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
 
+        Employee login_employee = (Employee) request.getSession().getAttribute("login_employee");
+
         int page;
-        try{
+        try {
             page = Integer.parseInt(request.getParameter("page"));
 
-        }catch(Exception e){
+        } catch (Exception e) {
             page = 1;
         }
-        List<Report> reports = em.createNamedQuery("getAllReports",Report.class)
-                                 .setFirstResult(15 * (page - 1))
-                                 .setMaxResults(15)
-                                 .getResultList();
-        long reports_count = (long)em.createNamedQuery("getReportsCount",Long.class)
-                                       .getSingleResult();
+        List<Report> reports = em.createNamedQuery("getMyAllReports", Report.class)
+                .setParameter("employee", login_employee)
+                .setFirstResult(15 * (page - 1))
+                .setMaxResults(15)
+                .getResultList();
+
+        long reports_count = (long) em.createNamedQuery("getMyReportsCount", Long.class)
+                .setParameter("employee", login_employee)
+                .getSingleResult();
 
         em.close();
 
         request.setAttribute("reports", reports);
         request.setAttribute("reports_count", reports_count);
         request.setAttribute("page", page);
-        if(request.getSession().getAttribute("flush") != null){
+
+        if (request.getSession().getAttribute("flush") != null) {
             request.setAttribute("flush", request.getSession().getAttribute("flush"));
             request.getSession().removeAttribute("flush");
 
         }
 
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/reports/index.jsp");
-        rd.forward(request,response);
+        rd.forward(request, response);
     }
 
 }
